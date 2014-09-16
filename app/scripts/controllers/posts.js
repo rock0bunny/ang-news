76307af698f5b7acea26701ee06c7e0828c01ac2
rock0bunny <rock0bunny@gmail.com>
2014-09-16 12:13:35 +0300

    navbar, filter

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -4,12 +4,6 @@ app.controller('PostsCtrl', function ($scope, $location, Post){
     $scope.post = {url: 'http://', title: ''};
     $scope.posts = Post.all;
 
-    $scope.submitPost = function() {
-        Post.create($scope.post).then(function(ref){
-            $location.path('/posts/' + ref.name());
-        });
-    };
-
     $scope.deletePost = function(post) {
         Post.delete(post);
     };
